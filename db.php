da619b2c29c4fe7ca44c4a5578af5c48cc60e54c
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-06-08 08:47:35 +0800

    remove db credentials

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
     defined("DB_HOST") ? null : define("DB_HOST", "localhost");
-    defined("DB_USER") ? null : define("DB_USER", "u652515858_raffle_loisv");
-    defined("DB_PASS") ? null : define("DB_PASS", "f&fgJzs!3fKJ216#");
-    defined("DB_NAME") ? null : define("DB_NAME", "u652515858_raffle");
+    defined("DB_USER") ? null : define("DB_USER", "root");
+    defined("DB_PASS") ? null : define("DB_PASS", "");
+    defined("DB_NAME") ? null : define("DB_NAME", "_raffle");
 
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 ?>
