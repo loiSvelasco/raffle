8523a8ccdcc4fa55a9f585cd1657e7154a773918
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-10-05 13:13:38 +0800

    update db details for demo

--- config.php
+++ config.php
@@ -2,14 +2,14 @@
 
 session_start();
 
-$_SESSION['DB_HOST'] = 'localhost';
-$_SESSION['DB_USER'] = 'root';
-$_SESSION['DB_PASS'] = '';
-$_SESSION['DB_NAME'] = 'raffle';
+// $_SESSION['DB_HOST'] = 'localhost';
+// $_SESSION['DB_USER'] = 'root';
+// $_SESSION['DB_PASS'] = '';
+// $_SESSION['DB_NAME'] = 'raffle';
 
 // ghetto way of doing things when server does not support composer ;-;
 
-// $_SESSION['DB_HOST'] = 'localhost';
-// $_SESSION['DB_USER'] = 'u652515858_raffle_loisv';
-// $_SESSION['DB_PASS'] = 'f&fgJzs!3fKJ216#';
-// $_SESSION['DB_NAME'] = 'u652515858_raffle';
+$_SESSION['DB_HOST'] = 'localhost';
+$_SESSION['DB_USER'] = 'u652515858_raffle_loisv';
+$_SESSION['DB_PASS'] = 'f&fgJzs!3fKJ216#';
+$_SESSION['DB_NAME'] = 'u652515858_raffle';
