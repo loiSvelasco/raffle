24b80bdfe6bc5fe7ea04790a32a5cde46877cc74
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-10-05 14:29:27 +0800

    set replaced dataset with fixed names

--- tests/valid_participants.php
+++ tests/valid_participants.php
@@ -2,7 +2,7 @@
 require_once '../functions.php';
 require_once 'dataset.config.php';
 
-$getNumValid = query("SELECT * FROM " . DATASET . " WHERE status = 'valid'");
+$getNumValid = query("SELECT * FROM participants_tests WHERE status = 'valid'");
 confirm($getNumValid);
 
 echo row_count($getNumValid);
