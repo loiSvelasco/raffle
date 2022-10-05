7ea55b68d06f321e160351634cfcfcf319b99a20
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-10-05 13:28:07 +0800

    set db to online

--- db.php
+++ db.php
@@ -1,9 +1,21 @@
 <?php
 
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'raffle');
-    
+    $online = true;
+
+    if($online) 
+    {
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'u652515858_raffle_loisv');
+        define('DB_PASS', 'f&fgJzs!3fKJ216#');
+        define('DB_NAME', 'u652515858_raffle');
+    }
+    else 
+    {
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'root');
+        define('DB_PASS', '');
+        define('DB_NAME', 'raffle');
+    }
+
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 ?>
